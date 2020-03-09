e5482a7f8615a9af668ce7eca7357f9b2360ce4d
Brady Wied <brady@bswtechconsulting.com>
2020-03-09 11:07:41 -0600

    Get arrange portion done

--- src/test/java/com/avioconsulting/jenkins/mule/impl/DesignCenterDeployerTest.groovy
+++ src/test/java/com/avioconsulting/jenkins/mule/impl/DesignCenterDeployerTest.groovy
@@ -4,6 +4,7 @@ import com.avioconsulting.jenkins.mule.impl.httpapi.HttpClientWrapper
 import com.avioconsulting.jenkins.mule.impl.models.FileBasedDeploymentRequest
 import io.vertx.core.Vertx
 import io.vertx.core.http.HttpServer
+import org.apache.commons.io.FileUtils
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
@@ -37,10 +38,35 @@ class DesignCenterDeployerTest implements HttpServerUtils {
     @Test
     void getRamlFilesFromApp_is_apikit() {
         // arrange
+        def tempDir = new File('target/temp')
+        def tempAppDirectory = new File(tempDir,
+                                        'designcenterapp')
+        tempAppDirectory.deleteDir()
+        tempAppDirectory.mkdirs()
+        def apiDirectory = new File(tempAppDirectory,
+                                    'api')
+        FileUtils.touch(new File(apiDirectory,
+                                 'stuff.yaml'))
+        def folder = new File(apiDirectory,
+                              'folder')
+        FileUtils.touch(new File(folder,
+                                 'lib.yaml'))
+        def exchangeModules = new File(apiDirectory,
+                                       'exchange_modules')
+        FileUtils.touch(new File(exchangeModules,
+                                 'junk'))
+        FileUtils.touch(new File(apiDirectory,
+                                 'exchange.json'))
+        def antBuilder = new AntBuilder()
+        def zipFile = new File(tempDir,
+                               'designcenterapp.zip')
+        FileUtils.deleteQuietly(zipFile)
+        antBuilder.zip(destfile: zipFile,
+                       basedir: tempAppDirectory)
         def request = new FileBasedDeploymentRequest() {
             @Override
             InputStream getApp() {
-                null
+                zipFile.newInputStream()
             }
         }
 
