8aa3a3d4760df77844bda68ec0c6258a8d2f8104
Brady Wied <brady@bswtechconsulting.com>
2020-03-19 13:51:21 -0600

    DRY out auto discovery stuff

--- src/test/resources/deploySpec.groovy
+++ src/test/resources/deploySpec.groovy
@@ -11,8 +11,10 @@ muleDeploy {
         }
         file 'path/to/file.jar'
         cryptoKey 'theKey'
-        anypointClientId 'the_client_id'
-        anypointClientSecret 'the_client_secret'
+        autodiscovery {
+            clientId 'the_client_id'
+            clientSecret 'the_client_secret'
+        }
         cloudHubAppPrefix 'AVI'
         appProperties([
                 someProp: 'someValue'
