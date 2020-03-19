a082df7a5ac7821e8a068c880226e4ed4c165c3f
Brady Wied <brady@bswtechconsulting.com>
2020-03-19 15:52:08 -0600

    Add policies and other things

--- src/test/resources/deploySpec.groovy
+++ src/test/resources/deploySpec.groovy
@@ -6,7 +6,56 @@ muleDeploy {
         organizationName 'ACME Brick'
     }
 
+    apiSpecification {
+        name 'Design Center Project Name'
+        // everything else in this closure is optional
+        exchangeAssetId 'the-asset-id'
+        apiMajorVersion 'v1'
+        mainRamlFile 'stuff.raml'
+        endpoint 'https://foobar'
+    }
+
+    policies {
+        policy {
+            assetId 'some-asset-id'
+            version '1.2.1'
+            config([
+                    hello: 123
+            ])
+            // paths is optional, will apply to entire API definition if omitted
+            paths {
+                path {
+                    method GET
+                    method PUT
+                    regex '.*foo'
+                }
+                path {
+                    method PUT
+                    regex '.*bar'
+                }
+            }
+            // by default, groupId is assumed to be Mulesoft's built-in policy group. Otherwise, you can do this
+            groupId 'stuff'
+            // OR use the same organization being used for deployment
+            groupId ourGroupId
+        }
+        clientEnforcementPolicyBasic {
+            // version is optional (will use version in this library by default)
+            version '1.2.1'
+            // can supply paths just like above if necessary
+        }
+    }
+
+    // this entire section is optional. if you omit it
+    enabledFeatures {
+        all
+        // or you can do this and specify a list of what you want enabled
+        DesignCenterSync
+        ApiManagerDefinitions
+    }
+
     cloudHubApplication {
+        appVersion '1.2.3'
         environment 'DEV'
         applicationName 'the-app'
         workerSpecs {
