2cbb512a610e40eeafd3b9a837113937b0c92b05
Brady Wied <brady@bswtechconsulting.com>
2020-03-19 12:54:32 -0600

    Outline what we might want

--- src/test/resources/deploySpec.groovy
+++ src/test/resources/deploySpec.groovy
@@ -1,5 +1,24 @@
+import com.avioconsulting.mule.deployment.api.models.AwsRegions
+import com.avioconsulting.mule.deployment.api.models.WorkerTypes
+
 muleDeploy {
     cloudHubApplication {
-        environment 'DEV'
+        environment'DEV'
+        applicationName 'the-app'
+        workerSpecs {
+            muleVersion '4.2.2'
+            usePersistentQueues true
+            workerType WorkerTypes.Micro
+            workerCount 1
+            awsRegion AwsRegions.UsEast1
+        }
+        file 'path/to/file.jar'
+        cryptoKey 'theKey'
+        anypointClientId 'the_client_id'
+        anypointClientSecret 'the_client_secret'
+        cloudHubAppPrefix 'AVI'
+        appProperties [
+                someProp: 'someValue'
+        ]
     }
 }
