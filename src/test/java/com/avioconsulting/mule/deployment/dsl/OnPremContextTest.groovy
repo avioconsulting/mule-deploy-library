f78270e55ee905c74f6f326122b598ae76be749e
Brady Wied <brady@bswtechconsulting.com>
2020-03-20 17:20:00 -0600

    Cover optional case

--- src/test/java/com/avioconsulting/mule/deployment/dsl/OnPremContextTest.groovy
+++ src/test/java/com/avioconsulting/mule/deployment/dsl/OnPremContextTest.groovy
@@ -3,8 +3,8 @@ package com.avioconsulting.mule.deployment.dsl
 import org.junit.Assert
 import org.junit.Test
 
-import static org.hamcrest.MatcherAssert.assertThat
 import static org.hamcrest.Matchers.*
+import static org.hamcrest.MatcherAssert.assertThat
 
 @SuppressWarnings(["UnnecessaryQualifiedReference", "GroovyAssignabilityCheck"])
 class OnPremContextTest {
@@ -43,11 +43,36 @@ class OnPremContextTest {
     @Test
     void with_optional() {
         // arrange
+        def context = new OnPremContext()
+        def closure = {
+            environment 'DEV'
+            applicationName 'the-app'
+            appVersion '1.2.3'
+            file 'path/to/file.jar'
+            targetServerOrClusterName 'server1'
+            appProperties foo: 'bar'
+        }
+        closure.delegate = context
+        closure.call()
 
         // act
+        def request = context.createDeploymentRequest()
 
         // assert
-        Assert.fail("write it")
+        request.with {
+            assertThat environment,
+                       is(equalTo('DEV'))
+            assertThat appName,
+                       is(equalTo('the-app'))
+            assertThat appVersion,
+                       is(equalTo('1.2.3'))
+            assertThat file,
+                       is(equalTo(new File('path/to/file.jar')))
+            assertThat targetServerOrClusterName,
+                       is(equalTo('server1'))
+            assertThat appProperties,
+                       is(equalTo([foo: 'bar']))
+        }
     }
 
     @Test
