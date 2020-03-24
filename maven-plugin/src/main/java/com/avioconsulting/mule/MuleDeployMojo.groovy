c987a3cb6279aea4fc09597cc6738fec67e60b3a
Brady Wied <brady@bswtechconsulting.com>
2020-03-24 08:57:02 -0600

    Add mojo skeleton

--- maven-plugin/src/main/java/com/avioconsulting/mule/MuleDeployMojo.groovy
+++ maven-plugin/src/main/java/com/avioconsulting/mule/MuleDeployMojo.groovy
@@ -1,20 +1,27 @@
 package com.avioconsulting.mule
 
+import com.avioconsulting.mule.deployment.api.DryRunMode
 import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugin.MojoExecutionException
 import org.apache.maven.plugin.MojoFailureException
 import org.apache.maven.plugins.annotations.Mojo
+import org.apache.maven.plugins.annotations.Parameter
 
 @Mojo(name = 'muleDeploy', requiresProject = false)
 class MuleDeployMojo extends AbstractMojo {
+    @Parameter(required = true, property = 'anypoint.username')
     private String anypointUsername
+    @Parameter(required = true, property = 'anypoint.password')
     private String anypointPassword
-    private String dryRunMode
+    @Parameter(defaultValue = 'Run', property = 'deploy.mode')
+    private DryRunMode dryRunMode
+    @Parameter(required = true, property = 'groovy.file')
     private File groovyFile
+    @Parameter(property = 'dsl.params')
     private Map<String, String> dslParams
 
     @Override
     void execute() throws MojoExecutionException, MojoFailureException {
-
+        this.log.info "would deploy with ${dryRunMode}"
     }
 }
