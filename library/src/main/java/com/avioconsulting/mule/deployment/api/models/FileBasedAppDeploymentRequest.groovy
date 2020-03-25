a572ef62b3cca00949569a03f76482066af1a3b6
Brady Wied <brady@bswtechconsulting.com>
2020-03-25 13:35:43 -0600

    DRY

--- library/src/main/java/com/avioconsulting/mule/deployment/api/models/FileBasedAppDeploymentRequest.groovy
+++ library/src/main/java/com/avioconsulting/mule/deployment/api/models/FileBasedAppDeploymentRequest.groovy
@@ -9,7 +9,6 @@ import java.nio.file.Path
 abstract class FileBasedAppDeploymentRequest {
     @Lazy
     protected PomInfo parsedPomProperties = {
-        println 'hi***************'
         def zipOrJarPath = getFile().toPath()
         FileSystems.newFileSystem(zipOrJarPath,
                                   null).withCloseable { fs ->
@@ -19,13 +18,16 @@ abstract class FileBasedAppDeploymentRequest {
             assert pomXmlPath: 'Was not able to find pom.xml in ZIP/JAR'
             def parser = new XmlParser().parseText(pomXmlPath.text)
             def props = [:]
+            def getLocalValue = { Node node ->
+                (node.value() as QName).localPart
+            }
             parser['properties'][0].each { Node node ->
                 def key = (node.name() as QName).localPart
-                props[key] = (node.value() as QName).localPart
+                props[key] = getLocalValue(node)
             }
-            return new PomInfo((parser['groupId'][0].value() as QName).localPart,
-                               (parser['artifactId'][0].value() as QName).localPart,
-                               (parser['version'][0].value() as QName).localPart,
+            return new PomInfo(getLocalValue(parser['groupId'][0] as Node),
+                               getLocalValue(parser['artifactId'][0] as Node),
+                               getLocalValue(parser['version'][0] as Node),
                                props)
         } as PomInfo
     }()
