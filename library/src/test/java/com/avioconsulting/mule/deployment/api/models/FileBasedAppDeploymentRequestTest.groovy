1ce02891cfe4e60795c487f9ee870305dbe2f3a6
Brady Wied <brady@bswtechconsulting.com>
2021-05-25 12:49:34 -0600

    Add directory parameter

--- library/src/test/java/com/avioconsulting/mule/deployment/api/models/FileBasedAppDeploymentRequestTest.groovy
+++ library/src/test/java/com/avioconsulting/mule/deployment/api/models/FileBasedAppDeploymentRequestTest.groovy
@@ -26,7 +26,7 @@ class FileBasedAppDeploymentRequestTest implements AppBuilding {
                                tempAppDirectory)
 
         // act
-        def result = request.getRamlFilesFromApp()
+        def result = request.getRamlFilesFromApp('/api')
 
         // assert
         assertThat result,
@@ -39,7 +39,7 @@ class FileBasedAppDeploymentRequestTest implements AppBuilding {
         def request = buildFullApp()
 
         // act
-        def result = request.getRamlFilesFromApp()
+        def result = request.getRamlFilesFromApp('/api')
                 .sort { item -> item.fileName } // consistent for test
 
         // assert
@@ -54,6 +54,23 @@ class FileBasedAppDeploymentRequestTest implements AppBuilding {
                                         'howdy1'),
                            new RamlFile('stuff.yaml',
                                         expectedStuffContents)
+                   ]))
+    }
+
+    @Test
+    void getRamlFilesFromApp_directory_specified() {
+        // arrange
+        def request = buildFullApp()
+
+        // act
+        def result = request.getRamlFilesFromApp('/api/folder')
+                .sort { item -> item.fileName } // consistent for test
+
+        // assert
+        assertThat result,
+                   is(equalTo([
+                           new RamlFile('lib.yaml',
+                                        'howdy1')
                    ]))
     }
 }
