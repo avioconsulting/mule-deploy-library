99584e8bd5c8bceee252d518aa447c96bef5eea9
Guilherme Teixeira <guilherme.teixeira@avioconsulting.com>
2022-11-29 15:49:08 -0600

    Added full list of parameters on DSL for cloudhub v2 version

--- maven-plugin/examples/mule4_project/muleDeploy_ch2.groovy
+++ maven-plugin/examples/mule4_project/muleDeploy_ch2.groovy
@@ -14,10 +14,8 @@ muleDeploy {
         }
     }
 
-    cloudHub2Application {
+    cloudHubV2Application {
         environment params.env
-        // the Maven plugin will automatically set `params.appArtifact` to the path of the target JAR if it is used
-        // in a project POM
         file params.appArtifact
         cryptoKey params.cryptoKey
         autoDiscovery {
@@ -28,13 +26,58 @@ muleDeploy {
 
         // Cloudhub v2 specific params
         uri 'https://anypoint.mulesoft.com'
-        provider 'MC'
-        target 'Cloudhub-US-West-2'
         muleVersion '4.4.0'
-        server 'anypoint-exchange-v3'
+        username 'Anypoint_user'
+        password 'Anypoint_pass'
+        authToken '4bb5-88e8'
+        connectedAppClientId 'f2ea2cb4'
+        connectedAppClientSecret 'c600'
+        connectedAppGrantType 'client_credentials'
         applicationName '${project.name}'
+        scopeLoggingConfigurations {
+            scopeLoggingConfiguration {
+                scope 'com.pkg.warning'
+                logLevel 'WARN'
+            }
+            scopeLoggingConfiguration {
+                scope 'com.pkg.debug'
+                logLevel 'DEBUG'
+            }
+        }
+        target 'Cloudhub-US-West-2'
+        provider 'MC'
+        environment 'DEV'
         replicas '1'
         vCores '0.1'
-        businessGroup 'Mule Sandbox'
+        businessGroup 'AVIO Sandbox'
+        businessGroupId 'f2ea2cb4-c600-4bb5-88e8-e952ff5591ee'
+        deploymentTimeout '1000000'
+        server 'anypoint-exchange-v3'
+        skip 'false'
+        skipDeploymentVerification 'false'
+        deploymentSettings {
+            lastMileSecurity 'false'
+            updateStrategy 'recreate'
+            clustered 'true'
+            http {
+                inbound {
+                    publicUrl 'myapp.anypoint.com'
+                    lastMileSecurity 'true'
+                    forwardSslSession 'true'
+                }
+            }
+            persistentObjectStore 'false'
+            jvm {
+                args '-XX:MaxMetaspaceSize=500m -XX:MaxRAMPercentage=60.0'
+            }
+            generateDefaultPublicUrl 'true'
+        }
+        integrations {
+            services {
+                objectStoreV2 {
+                    enabled 'true'
+                }
+            }
+        }
     }
 }
