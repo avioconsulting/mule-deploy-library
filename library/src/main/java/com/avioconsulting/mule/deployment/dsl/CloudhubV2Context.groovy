2794241bca1157324a8b1334fd30e8ae8129a33c
Guilherme Teixeira <guilherme.teixeira@avioconsulting.com>
2022-12-19 03:51:50 -0600

    Added business group to DSL

--- library/src/main/java/com/avioconsulting/mule/deployment/dsl/CloudhubV2Context.groovy
+++ library/src/main/java/com/avioconsulting/mule/deployment/dsl/CloudhubV2Context.groovy
@@ -3,7 +3,7 @@ package com.avioconsulting.mule.deployment.dsl
 import com.avioconsulting.mule.deployment.api.models.CloudhubV2DeploymentRequest
 
 class CloudhubV2Context extends BaseContext {
-    String environment, applicationName, appVersion, file, cryptoKey, cloudHubAppPrefix
+    String environment, applicationName, appVersion, file, cryptoKey, cloudHubAppPrefix, businessGroupId
     // make API visualizer, etc. more easy by default
     private WorkerV2SpecContext workerSpecs = new WorkerV2SpecContext()
     private AutodiscoveryContext autoDiscovery = new AutodiscoveryContext()
@@ -29,12 +29,13 @@ class CloudhubV2Context extends BaseContext {
                                       this.cloudHubAppPrefix,
                                       this.applicationName,
                                       this.appVersion,
+                                      this.businessGroupId,
                                       this.appProperties,
                                       this.otherCloudHubProperties)
     }
 
     @Override
     List<String> findOptionalProperties() {
-        ['appVersion', 'applicationName']
+        ['appVersion', 'applicationName', 'businessGroupId']
     }
 }
