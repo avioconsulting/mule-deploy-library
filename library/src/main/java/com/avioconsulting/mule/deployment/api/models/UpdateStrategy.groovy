44337b3c2134583f57bc812110a415c5e43b2cfe
Guilherme Teixeira <guilherme.teixeira@avioconsulting.com>
2023-01-25 07:20:55 -0600

    Comments on UpdateStrategy updated

--- library/src/main/java/com/avioconsulting/mule/deployment/api/models/UpdateStrategy.groovy
+++ library/src/main/java/com/avioconsulting/mule/deployment/api/models/UpdateStrategy.groovy
@@ -2,11 +2,13 @@ package com.avioconsulting.mule.deployment.api.models
 
 enum UpdateStrategy {
     /**
-     * 0.1 vCores
+     * Maintains availability by updating replicas incrementally.
+     * Requires one additional replica’s worth of resources to succeed.
      */
     rolling,
     /**
-     * 0.2 vCores
+     * Terminates replicas before re-deployment.
+     * Re-deployment is quicker than rolling and doesn’t require additional resources.
      */
     recreate
 }
