94fb6bccfc0a450b5c163b6d9d7a59bfe3a9157a
Talysson Castro <talyssoncastro@gmail.com>
2023-06-28 11:57:15 -0300

    feat: Add method to retrieve existing API in the interface IApiManagerDeployer.groovy

--- library/src/main/java/com/avioconsulting/mule/deployment/internal/subdeployers/IApiManagerDeployer.groovy
+++ library/src/main/java/com/avioconsulting/mule/deployment/internal/subdeployers/IApiManagerDeployer.groovy
@@ -4,12 +4,20 @@ import com.avioconsulting.mule.deployment.internal.models.ApiSpec
 import com.avioconsulting.mule.deployment.internal.models.ExistingApiSpec
 
 interface IApiManagerDeployer {
-/**
- * Creates a definition if it does not exist and updates if it does
- * @param desiredApiManagerDefinition - desired specs
- * @param appVersion - version of application being deployed
- * @return Updated, looked up API definition with ID
- */
+    /**
+     * Creates a definition if it does not exist and updates if it does
+     * @param desiredApiManagerDefinition - desired specs
+     * @param appVersion - version of application being deployed
+     * @return Updated, looked up API definition with ID
+     */
     ExistingApiSpec synchronizeApiDefinition(ApiSpec desiredApiManagerDefinition,
                                              String appVersion)
+
+    /**
+     * Retrieves the API specification from the API manager server based on asset ID and environment. If more than one
+     * API definition is found, will use the apiMajorVersion and instanceLabel to define which one is correct.
+     * @param desiredApiManagerDefinition - desired specification
+     * @return Existing API from API manager server. If does not exist, will return null
+     */
+    ExistingApiSpec getExistingApiDefinition(ApiSpec desiredApiManagerDefinition)
 }
