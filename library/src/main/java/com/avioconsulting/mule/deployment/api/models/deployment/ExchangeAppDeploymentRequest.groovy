dacc871d23a6ab94eeba028d680e3ac320e1613b
Talysson Castro <talyssoncastro@gmail.com>
2023-07-06 21:00:49 -0300

    feat: Move DesignCenter as unsupported to the proper class - ExchangeAppDeploymentRequest

--- library/src/main/java/com/avioconsulting/mule/deployment/api/models/deployment/ExchangeAppDeploymentRequest.groovy
+++ library/src/main/java/com/avioconsulting/mule/deployment/api/models/deployment/ExchangeAppDeploymentRequest.groovy
@@ -1,5 +1,6 @@
 package com.avioconsulting.mule.deployment.api.models.deployment
 
+import com.avioconsulting.mule.deployment.api.models.Features
 import com.avioconsulting.mule.deployment.internal.models.RamlFile
 
 abstract class ExchangeAppDeploymentRequest extends AppDeploymentRequest {
@@ -11,5 +12,10 @@ abstract class ExchangeAppDeploymentRequest extends AppDeploymentRequest {
     @Override
     List<RamlFile> getRamlFilesFromApp(String rootRamlDirectory, boolean ignoreExchange) {
         return null
+    }
+
+    @Override
+    List<Features> getUnsupportedFeatures() {
+        return [Features.DesignCenterSync]
     }
 }
