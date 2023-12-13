c5b7bbeb2fe7b0ca04c4ca72ed414eff802c7f19
Talysson de Castro <talyssoncastro@gmail.com>
2023-12-13 12:56:41 -0300

    CHv2 Tests  (#82)
    * Resolve conflicts
    
    * feat: Validate if mainRamlFile exists on separated method
    
    * feat: Support API manager and Policies deploy when using CH 2.0 and RTF deployment
    
    * refactor: Use base class AppDeploymentRequest.groovy and create getUnsupportedFeatures
    
    * feat: Return null for RAML files for ExchangeAppDeploymentRequest and derived classes
    
    * feat: Move DesignCenter as unsupported to the proper class - ExchangeAppDeploymentRequest
    
    * feat: Improve parameter name
    
    * docs: Improve examples
    
    * docs: Add example of CH 2.0 with API Manager and Policy sync
    
    * feat: Encapsulate logic of testing null on RAML files
    
    * feat: Add autoDiscoveryId property into deployment properties for RTF and CH 2.0
    
    * Use ApplicationName in tests
    
    * Requires appName for RTF and CH 2.0 applications
    
    * Fix CloudHub integration test
    
    * test: Unit test about validating empty name for RTF and CH 2.0 apps

--- library/src/test/java/com/avioconsulting/mule/deployment/api/models/CloudhubV2DeploymentRequestTest.groovy
+++ library/src/test/java/com/avioconsulting/mule/deployment/api/models/CloudhubV2DeploymentRequestTest.groovy
@@ -75,6 +75,66 @@ class CloudhubV2DeploymentRequestTest implements MavenInvoke {
      * getCloudhubAppInfo() method.
      */
     @Test
+    void test_getCloudhubAppInfo_only_required() {
+
+        def request = new CloudhubV2DeploymentRequest('DEV',
+                                                    new WorkerSpecRequest('us-west-2', '4.3.0'),
+                                                    'theKey',
+                                                    'theClientId',
+                                                    'theSecret',
+                                                    new ApplicationName('new-app',false,false,null,null),
+                                                    '2.2.9',
+                                                    'f2ea2cb4-c600-4bb5-88e8-e952ff5591ee')
+
+        def appInfo = request.getCloudhubAppInfo()
+
+        assertThat appInfo,
+                   is(equalTo([
+                           name: 'new-app',
+                           application: [
+                                   ref: [
+                                           groupId: 'f2ea2cb4-c600-4bb5-88e8-e952ff5591ee',
+                                           artifactId : 'new-app',
+                                           version: '2.2.9',
+                                           packaging: "jar"
+                                   ],
+                                   desiredState: "STARTED",
+                                   configuration: [
+                                           "mule.agent.application.properties.service": [
+                                                   applicationName: 'new-app',
+                                                   properties: [:]
+                                           ]
+                                   ],
+                                   integrations:[
+                                           services : [
+                                                   objectStoreV2: [
+                                                           enabled: false
+                                                   ]
+                                           ]
+                                   ],
+                                   objectStoreV2Enabled: false,
+                                   "vCores": VCoresSize.vCore1GB.vCoresSize
+                           ],
+                           target: [
+                                   targetId: null,
+                                   provider: "MC",
+                                   deploymentSettings: [
+                                           runtimeVersion: '4.3.0',
+                                           lastMileSecurity: false,
+                                           persistentObjectStore: false,
+                                           clustered: false,
+                                           updateStrategy: UpdateStrategy.rolling,
+                                           enforceDeployingReplicasAcrossNodes: false,
+                                           forwardSslSession: false,
+                                           disableAmLogForwarding: true,
+                                           generateDefaultPublicUrl: false
+                                   ],
+                                   replicas: 1
+                           ]
+                   ]))
+    }
+
+    @Test
     void test_deploymentRequest_getCloudhubAppInfo_ok() {
 
         def request = new CloudhubV2DeploymentRequest('DEV',
@@ -174,6 +234,27 @@ class CloudhubV2DeploymentRequestTest implements MavenInvoke {
 
         MatcherAssert.assertThat exception.message,
                 is(equalTo("Maximum size of application name is 42 and the provided name has 53 characters"))
+    }
+
+    @Test
+    void test_deploymentRequest_appName_empty() {
+
+        def appName = null
+        def cloudHubPrefix = 'client'
+        def environment = 'DEV'
+        def exception = shouldFail {
+            new CloudhubV2DeploymentRequest(environment,
+                    new WorkerSpecRequest('us-west-2'),
+                    'theKey',
+                    'theClientId',
+                    'theSecret',
+                    new ApplicationName(appName, false, false, null, null),
+                    '4.2.2',
+                    'f2ea2cb4-c600-4bb5-88e8-e952ff5591ee')
+        }
+
+        MatcherAssert.assertThat exception.message,
+                is(equalTo("Property applicationName.baseAppName is required for CloudHub 2.0 and RTF applications"))
     }
 
 }
